af671cdf65d0bbf5860767a4bf367d1bbe19eef9
Rudra Patel <rudrapatel648673@gmail.com>
2026-06-02 11:53:07 +0530

    Connected to MongoDB Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express();
+const connectDB = require('./DB/connect')
+require('dotenv').config()
 
-app.listen(3000 , ()=>{
-    console.log('Server is Listening ...')
-})
+const port = process.env.PORT
+
+try{
+    connectDB(process.env.MONGO_URI)
+    app.listen(3000 , ()=>{
+        console.log('Server is Listening ...')
+    })
+}
+catch(err){
+    console.log(err)
+}
